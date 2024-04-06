34c887cf550080bf1985fb9ac4c2262de63fcbb4
hyunjin <guswlsdl0121@gmail.com>
2024-04-06 23:28:08 +0900

    feat: 스웨거 config 파일 작성, 스웨거 작성 예시 추가

---
+++ src/main/java/com/tangtang/polingo/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.tangtang.polingo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Polingo API 명세",
+                description = "Polingo API 명세서 입니다. ",
+                version = "v1")
+        , servers = {
+        @Server(url = "/", description = "API 서버"),
+}
+)
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement));
+    }
+}
--- src/main/java/com/tangtang/polingo/global/controller/GlobalController.java
+++ src/main/java/com/tangtang/polingo/global/controller/GlobalController.java
@@ -1,12 +1,17 @@
 package com.tangtang.polingo.global.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "전역 API", description = "특정 도메인에 종속되지 않는 API들입니다.")
 @RestController
 public class GlobalController {
-    @GetMapping("/health-check")
+
+    @Operation(summary = "Health-Check API", description = "서버가 정상적으로 구동되는지 확인, 성공시 200 반환")
+    @GetMapping("/api/health-check")
     public HttpStatus healthCheck() {
         return HttpStatus.OK;
     }
