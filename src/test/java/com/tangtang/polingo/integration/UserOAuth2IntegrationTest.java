f7836eb8320d12c9c12f3982bde5b85849165803
minturtle <minseok0425@kakao.com>
2024-04-07 13:27:13 +0900

    Feat : application-test.yml 추가

---
+++ src/test/java/com/tangtang/polingo/integration/UserOAuth2IntegrationTest.java
@@ -0,0 +1,14 @@
+package com.tangtang.polingo.integration;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserOAuth2IntegrationTest {
+
+
+
+
+}
