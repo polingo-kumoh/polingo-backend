6181d06427dde5d3b978397b5cae90db033bb420
hyunjin <guswlsdl0121@gmail.com>
2024-04-09 03:30:12 +0900

    chore: 파일명 오타 수정

--- script/config.sh
+++ script/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 애플리케이션 이름 및 경로 설정
-APP_NAME="/home/ubuntu/polingo/build/libs/poling-application.jar"
+APP_NAME="/home/ubuntu/polingo/build/libs/polingo-application.jar"
 
 # 로그 파일 디렉토리 설정
 LOG_DIR="/home/ubuntu/polingo"
