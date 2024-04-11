a657cf123434cd423c6a0abca5e19ee02f6d0c2a
minturtle <minseok0425@kakao.com>
2024-04-11 19:21:29 +0900

    Feat : 이미지 파일 검증 기능 추가

--- src/main/java/com/tangtang/polingo/translate/controller/TranslateController.java
+++ src/main/java/com/tangtang/polingo/translate/controller/TranslateController.java
@@ -8,8 +8,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 @Tag(name = "번역 관련 API", description = "번역 관련 API를 제공합니다.")
 @Slf4j
@@ -34,9 +36,17 @@ public class TranslateController {
             @RequestParam Language sourceLanguage,
             @RequestParam("file") MultipartFile image
     ) {
+        if (!isImageFile(image)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미지 파일만 업로드 가능합니다.");
+        }
+
         return TranslateResponse.builder().originalText("Hello").translatedText("안녕하세요.").build();
     }
 
 
-
+    // 이미지 파일인지 확인하는 메서드
+    private boolean isImageFile(MultipartFile file) {
+        String contentType = file.getContentType();
+        return contentType != null && contentType.startsWith("image/");
+    }
 }
