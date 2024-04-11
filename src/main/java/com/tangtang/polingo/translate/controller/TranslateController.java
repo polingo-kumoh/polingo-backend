cebb39a773422f6dd17b70cfd5dcfec9b491b237
minturtle <minseok0425@kakao.com>
2024-04-11 19:19:43 +0900

    Feat : 이미지 번역 API 추가

--- src/main/java/com/tangtang/polingo/translate/controller/TranslateController.java
+++ src/main/java/com/tangtang/polingo/translate/controller/TranslateController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "번역 관련 API", description = "번역 관련 API를 제공합니다.")
 @Slf4j
@@ -27,7 +28,14 @@ public class TranslateController {
         return TranslateResponse.builder().originalText("Hello").translatedText("안녕하세요.").build();
     }
 
-
+    @PostMapping("/image")
+    @Operation(summary = "이미지 번역 API", description = "이미지를 번역합니다.")
+    public TranslateResponse translateImage(
+            @RequestParam Language sourceLanguage,
+            @RequestParam("file") MultipartFile image
+    ) {
+        return TranslateResponse.builder().originalText("Hello").translatedText("안녕하세요.").build();
+    }
 
 
 
