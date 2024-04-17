5fa818c1f58167fba95f30f2bfd97b49b6a8e43c
minturtle <minseok0425@kakao.com>
2024-04-17 14:39:47 +0900

    Test : 인증된 사용자는 이미지 파일을 가지고 번역을 수행해 번역본을 제공받을 수 있다.

--- src/test/java/com/tangtang/polingo/integration/TranslateIntegrationTest.java
+++ src/test/java/com/tangtang/polingo/integration/TranslateIntegrationTest.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.tangtang.polingo.global.constant.Language;
 import com.tangtang.polingo.security.service.JwtService;
+import com.tangtang.polingo.testutils.MockServerSetUpUtils;
+import com.tangtang.polingo.testutils.TestGoogleProperties;
+import com.tangtang.polingo.testutils.TestKakaoProperties;
 import com.tangtang.polingo.translate.dto.PlainTextTranslateRequest;
 import com.tangtang.polingo.translate.dto.TranslateResponse;
 import com.tangtang.polingo.translate.service.ocr.ImageToText;
@@ -18,7 +21,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
@@ -39,6 +45,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @Transactional
+@Import(TranslateIntegrationTest.TestConfig.class)
 @Sql(scripts = {"classpath:sql/init_user.sql"})
 @ExtendWith(MockitoExtension.class)
 public class TranslateIntegrationTest {
@@ -52,9 +59,6 @@ public class TranslateIntegrationTest {
     @MockBean
     SpeechClient speechClient;
 
-    @MockBean
-    ImageToText imageToText;
-
 
     @Autowired
     private MockMvc mockMvc;
@@ -64,6 +68,10 @@ public class TranslateIntegrationTest {
 
     @Autowired
     private JwtService jwtService;
+
+    @Autowired
+    private MockServerSetUpUtils mockServerSetUpUtils;
+
 
     @Test
     @DisplayName("인증된 사용자는 텍스트 원문을 가지고 번역을 수행해 번역본을 제공받을 수 있다.")
@@ -128,6 +136,63 @@ public class TranslateIntegrationTest {
 
     }
 
+    @Test
+    @DisplayName("인증된 사용자는 이미지 파일을 가지고 번역을 수행해 번역본을 제공받을 수 있다.")
+    void given_AuthenticatedUserOriginImage_when_Translate_then_ReturnOriginTextAndTranslatedText() throws Exception{
+        // given
+        String givenToken = jwtService.createToken(testUser);
 
+        MockMultipartFile imageFile =
+                new MockMultipartFile("image", "test-image.jpg", "image/jpeg", "test image data".getBytes());
+        TranslateResponse expectedResponse = new TranslateResponse("Hello, world!", "안녕, 세계!");
+
+
+
+        mockServerSetUpUtils.setUpOCRMockServer(imageFile);
+        when(translator.translateText("Hello, world!", "en", "ko"))
+                .thenReturn(new TextResult("안녕, 세계!", "en"));
+
+        // when
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/api/translate/image")
+                        .file("image", imageFile.getBytes())
+                        .param("source-type", Language.ENGLISH.toString())
+                        .header("Authorization", "Bearer " + givenToken)
+                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+        // then
+        TranslateResponse actualResponse = objectMapper
+                .readValue(result.getResponse().getContentAsString(), TranslateResponse.class);
+
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+
+        mockServerSetUpUtils.close();
+    }
+
+
+    @TestConfiguration
+    public static class TestConfig {
+
+        @Bean
+        public TestGoogleProperties testGoogleProperties() {
+            return new TestGoogleProperties();
+        }
+
+
+        @Bean
+        public TestKakaoProperties testKakaoProperties() {
+            return new TestKakaoProperties();
+        }
+
+        @Bean
+        public MockServerSetUpUtils mockServerSetUpUtils(
+                TestGoogleProperties testGoogleProperties,
+                TestKakaoProperties testKakaoProperties
+
+        ) {
+            return new MockServerSetUpUtils(testGoogleProperties, testKakaoProperties);
+        }
+
+    }
 
 }
