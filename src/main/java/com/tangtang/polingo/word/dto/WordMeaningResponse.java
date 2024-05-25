07b81255b87e5644bc8ed3cd9b3b5bc8660d2297
hyunjin <guswlsdl0121@gmail.com>
2024-05-25 22:10:22 +0900

    fix: 프론트 요구사항 반영
    - 단어 검색시 단어 뜻도 같이 반환하도록 변경
    - 단어 없으면 400Error 반환하도록 변경

--- src/main/java/com/tangtang/polingo/word/dto/WordMeaningResponse.java
+++ src/main/java/com/tangtang/polingo/word/dto/WordMeaningResponse.java
@@ -3,5 +3,5 @@ package com.tangtang.polingo.word.dto;
 import lombok.Builder;
 
 @Builder
-public record WordMeaningResponse(String word, String description) {
+public record WordMeaningResponse(String word, String translate, String description) {
 }
--- src/main/java/com/tangtang/polingo/word/service/WordService.java
+++ src/main/java/com/tangtang/polingo/word/service/WordService.java
@@ -5,11 +5,13 @@ import com.tangtang.polingo.word.dto.WordMeaningResponse;
 import com.tangtang.polingo.word.service.search.WordSearcher;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class WordService {
     private final WordSearcher wordSearcher;
@@ -17,11 +19,28 @@ public class WordService {
     public WordMeaningResponse findWordMeaning(String code, String word) {
         Language language = Language.fromCode(code);
         Optional<String> descriptionOpt = wordSearcher.search(language, word);
-        String description = descriptionOpt.orElse("단어가 사전에 존재하지 않습니다!");
+
+        if (descriptionOpt.isEmpty()) {
+            throw new IllegalArgumentException("단어가 사전에 존재하지 않습니다!");
+        }
+
+        String description = descriptionOpt.get();
+        String translate = extractFirstWord(description);
 
         return WordMeaningResponse.builder()
                 .word(word)
+                .translate(translate)
                 .description(description)
                 .build();
     }
+
+    private String extractFirstWord(String description) {
+        String cleanedDescription = description.replaceAll("[^가-힣 ]", "").strip();
+        String[] tokens = cleanedDescription.split(" ");
+        if (tokens.length > 0) {
+            return tokens[0];
+        }
+        return "";
+    }
 }
+
