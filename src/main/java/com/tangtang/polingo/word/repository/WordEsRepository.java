10e12a8810bfff38ddbcda77d2c15f6850e2bb95
hyunjin <guswlsdl0121@gmail.com>
2024-06-04 12:38:07 +0900

    fix: cors 주소 추가

--- src/main/java/com/tangtang/polingo/word/repository/WordEsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tangtang.polingo.word.repository;
-
-import com.tangtang.polingo.word.entity.WordDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface WordEsRepository extends ElasticsearchRepository<WordDocument, String> {
-}
